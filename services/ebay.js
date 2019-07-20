498b3d20aeb9cba91f351e3cabc38902c3267f49
Dayne <daynewr@gmail.com>
2019-07-19 23:49:43 -0500

    fixed ebay to wait for image load

--- services/ebay.js
+++ services/ebay.js
@@ -1,37 +1,69 @@
 const cheerio = require('cheerio');
-const request = require('request');
+const puppeteer = require('puppeteer');
 
-exports.getItems = search => {
-    return new Promise(resolve => {
-        request(`https://www.ebay.com/sch/i.html?_nkw=${search}&LH_BIN=1&_sop=12`,
-        (err, response, html) => {
-            const $ = cheerio.load(html);
+exports.getItems = async (search) => {
+    const browser = await puppeteer.launch({
+        args: [
+           '--no-sandbox',
+           '--disable-setuid-sandbox',
+         ]
+       });
+    const page = await browser.newPage();
+    await page.goto(`https://www.ebay.com/sch/i.html?_nkw=${search}&LH_BIN=1&_sop=12`, {waitUntil: 'networkidle0'});
+    await page.setViewport({
+        width: 1200,
+        height: 800
+    });
 
-            let results = [];
+    await autoScroll(page);
 
-            $('.s-item').each((i, element) => {
-                const image = $(element).find('.s-item__image-img').attr('src');
-                const price = $(element).find('.s-item__price').text().replace('$', '');
-                const shipping = $(element).find('.s-item__shipping').text();
-                const link = $(element).find('.s-item__link').attr('href');
-                const title = $(element).find('.s-item__title').text().trim();
-                const description = $(element).find('.s-item__subtitle').text().trim();
-                // const id = $(element).find('.s-item__link').attr('data-track');
+    const html = await page.content();
+    const $ = cheerio.load(html);
 
-                if(title.toUpperCase() !== 'SPONSORED') {
-                    results.push({
-                        image,
-                        price,
-                        shipping,
-                        link,
-                        title,
-                        description,
-                        source: 'ebay'
-                    });
-                }
+    let results = [];
+
+    $('.s-item').each((i, element) => {
+        const image = $(element).find('.s-item__image-img').attr('src');
+        const price = $(element).find('.s-item__price').text().replace('$', '');
+        const shipping = $(element).find('.s-item__shipping').text();
+        const link = $(element).find('.s-item__link').attr('href');
+        const title = $(element).find('.s-item__title').text().trim();
+        const description = $(element).find('.s-item__subtitle').text().trim();
+        // const id = $(element).find('.s-item__link').attr('data-track');
+
+        if(!title.toUpperCase().includes('SPONSORED') && !title.toUpperCase().includes('NEW LISTING')) {
+            results.push({
+                image,
+                price,
+                shipping,
+                link,
+                title,
+                description,
+                source: 'ebay'
             });
+        }
+    });
 
-            resolve(results);
+    await browser.close();
+    console.warn(results);
+    return results;
+};
+
+async function autoScroll(page){
+    await page.evaluate(async () => {
+        await new Promise((resolve, reject) => {
+            var totalHeight = 0;
+            var distance = 100;
+            var timer = setInterval(() => {
+                var scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+
+                if(totalHeight >= scrollHeight){
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 100);
         });
     });
-};
+}
